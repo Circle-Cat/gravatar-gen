988396baa7de43aebf35bc5b7b2ba8439ec5c280
Jingyuan Liang <jingyuanliang@circlecat.org>
2025-04-05 23:07:46 +0000

    Initial version
    Change-Id: Ifabfedca06ae2384bbbc2654bdc2700c3d2bcede

---
+++ cmd/gravatar-gen/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	avatarDir   = "avatar"
+	gravatarDir = "gravatar"
+)
+
+var suffixes = []string{
+	"@circlecat.org",
+	"@u.circlecat.org",
+}
+
+// https://docs.gravatar.com/api/avatars/go/
+func gravatarHash(email string) string {
+	hasher := sha256.Sum256([]byte(strings.TrimSpace(email)))
+	return hex.EncodeToString(hasher[:])
+}
+
+func main() {
+	err := os.MkdirAll(gravatarDir, 0o755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	files, err := os.ReadDir(avatarDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			log.Printf("Skipping directory %q", file.Name())
+			continue
+		}
+
+		targets := make([]string, 0, len(suffixes))
+		if file.Name() == "404.html" {
+			targets = append(targets, file.Name())
+		} else {
+			name := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
+
+			for _, suffix := range suffixes {
+				email := name + suffix
+				hash := gravatarHash(email)
+				log.Printf("%q translated to %q", email, hash)
+				targets = append(targets, hash)
+			}
+		}
+
+		avatar, err := os.ReadFile(filepath.Join(avatarDir, file.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, target := range targets {
+			err := os.WriteFile(filepath.Join(gravatarDir, target), avatar, 0o644)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("%q copied to %q", file.Name(), target)
+		}
+	}
+}
